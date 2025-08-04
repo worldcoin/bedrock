9d3e4d5cd197df72358825e5af841dfedf48fd90
pd <paolodamico@users.noreply.github.com>
2025-08-03 20:24:54 -0700

    extra typo

--- bedrock/src/transaction/contracts/mod.rs
+++ bedrock/src/transaction/contracts/mod.rs
@@ -1,4 +1,4 @@
 //! This module introduces contract definitions for all the smart contracts
-//! that power the commont transactions for the crypto wallet.
+//! that power the common transactions for the crypto wallet.
 
 pub mod erc20;
