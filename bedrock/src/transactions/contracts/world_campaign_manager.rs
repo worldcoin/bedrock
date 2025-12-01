7afe19c729a612551b786348f5f138b6cf3c0c6c
aurel-fr <105201452+aurel-fr@users.noreply.github.com>
2025-12-01 14:00:19 -0800

    chore: campaign manager addr

--- bedrock/src/transactions/contracts/world_campaign_manager.rs
+++ bedrock/src/transactions/contracts/world_campaign_manager.rs
@@ -21,7 +21,7 @@ pub fn world_campaign_manager_address() -> Address {
             address!("0xF19Bf1F55ce9466c90DC1cF4781362D89547dab5") // https://github.com/worldcoin/worldcoin-gift-contracts/pull/2
         }
         BedrockEnvironment::Production => {
-            address!("0xD61F9411E768871ca9bc723afC5fF3A4f731D0C1") // TODO replace with post-audit contract
+            address!("0x7A529297dd4659291Cb33EA732073FF8067224B5")
         }
     }
 }
