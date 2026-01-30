814d5c9d82bd7a0a5c1f6f609ffb337e5fb31394
Tom Waite <thomas.waite4@gmail.com>
2026-01-30 13:41:13 -0800

    feat: copy over migration code from oxide

---
+++ bedrock/src/migration/processors/mod.rs
@@ -0,0 +1,8 @@
+/// Example migration processors
+///
+/// This module contains skeleton implementations of migration processors
+/// that can be used as templates for actual migrations.
+pub mod example_processor;
+
+// Re-export for convenience
+pub use example_processor::ExampleProcessor;
