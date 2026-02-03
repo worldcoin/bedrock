80be7a9362fb2513b9322b7ee55c195da7df05ee
pd <paolodamico@users.noreply.github.com>
2026-02-02 19:07:13 -0800

    address feedback

--- bedrock/src/migration/error.rs
+++ bedrock/src/migration/error.rs
@@ -17,10 +17,25 @@ pub enum MigrationError {
     /// Device key-value store error
     #[error(transparent)]
     DeviceKeyValueStoreError(#[from] KeyValueStoreError),
+
+    /// Unexpected UniFFI callback error
+    #[error("unexpected error in foreign callback: {0}")]
+    UnexpectedUniFFICallbackError(String),
 }
 
 impl From<serde_json::Error> for MigrationError {
     fn from(error: serde_json::Error) -> Self {
         Self::SerdeJsonError(error.to_string())
+    }
+}
+
+/// Converts unexpected UniFFI callback errors to `FileSystemError`.
+///
+/// This implementation is required for foreign trait support.
+///
+/// Without this implementation, unexpected foreign errors would panic the Rust code.
+impl From<uniffi::UnexpectedUniFFICallbackError> for MigrationError {
+    fn from(error: uniffi::UnexpectedUniFFICallbackError) -> Self {
+        Self::UnexpectedUniFFICallbackError(error.reason)
     }
 }
